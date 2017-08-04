9ed102e4c60302e9019622a808c217543d77fcf8
Finley <galoislee@yahoo.com.tw>
2017-08-04 10:52:57 +0800

    資料結構演算法課程說明

---
+++ C/作業程式/basic_repetition_1.cpp
@@ -0,0 +1,16 @@
+// 資料結構 演算法 課程說明 
+// 基礎邏輯 重複
+
+#include<stdio.h>
+
+int main()
+{
+	int sum;
+	for(int count = 1 ; count <= 10 ; count++)
+	{
+		sum = sum+count;
+	}
+	
+	printf("數字1-10的總和:%d",sum);
+	
+}
